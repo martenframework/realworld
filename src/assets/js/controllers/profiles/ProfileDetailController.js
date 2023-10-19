e1f807965120d0a86d7185a03babec4eaf3ae574
Morgan Aubert <morgan@aubert.email>
2023-10-18 22:12:13 -0400

    Ensure articles showcased in the profile can be favorited

--- src/assets/js/controllers/profiles/ProfileDetailController.js
+++ src/assets/js/controllers/profiles/ProfileDetailController.js
@@ -1,8 +1,10 @@
+import initFavoriteButtons from '../../core/initFavoriteButtons';
 import initFollowButtons from '../../core/initFollowButtons';
 
 export default {
   async init() {
     const profilePageDiv = document.getElementById('profile_page');
     await initFollowButtons(profilePageDiv.dataset.username);
+    await initFavoriteButtons();
   },
 };
