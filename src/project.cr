c4acc14870a9f121ecce1f87f8040a6c5f522ad1
Morgan Aubert <morgan@aubert.email>
2023-10-21 11:27:18 -0400

    Update requirements

--- src/project.cr
+++ src/project.cr
@@ -4,14 +4,14 @@ require "marten"
 require "marten_auth"
 require "sqlite3"
 
-# Project requirements.
-require "./handlers/**"
-require "./apps/auth/app"
-require "./apps/blogging/app"
-require "./apps/profiles/app"
-
 # Configuration requirements.
 require "../config/routes"
 require "../config/settings/base"
 require "../config/settings/**"
 require "../config/initializers/**"
+
+# Project requirements.
+require "./handlers/**"
+require "./apps/auth/app"
+require "./apps/blogging/app"
+require "./apps/profiles/app"
