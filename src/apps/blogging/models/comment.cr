56e9151fb45531eb241b81b5db2c488479a6898e
Morgan Aubert <morgan@aubert.email>
2023-11-18 17:52:44 -0500

    Add missing reverse relation in Comment model

--- src/apps/blogging/models/comment.cr
+++ src/apps/blogging/models/comment.cr
@@ -2,7 +2,7 @@ module Blogging
   class Comment < Marten::Model
     field :id, :big_int, primary_key: true, auto: true
     field :body, :text
-    field :article, :many_to_one, to: Blogging::Article
+    field :article, :many_to_one, to: Blogging::Article, related: :comments
     field :author, :many_to_one, to: Profiles::Profile
 
     with_timestamp_fields
